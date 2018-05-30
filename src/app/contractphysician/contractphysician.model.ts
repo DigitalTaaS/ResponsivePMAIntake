beb8ae7593e5272f73689b1f25c1b73fe2462548
ramaagur <ramaagurusamy@outlook.com>
2018-05-30 11:36:13 -0500

    Interceptor and Model

---
+++ src/app/contractphysician/contractphysician.model.ts
@@ -0,0 +1,85 @@
+export class ContractphysicianModel {
+    //Demographics Panel    
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    suffix: string;
+    alias: string;
+    gender: string;
+    dateOfBirth: string;
+
+    //Licensing Panel
+    contractedPartners: ContractedPartner[];
+
+    //Licensing Panel
+    npi: string;
+    dea: string;
+    licenseNumber: string;
+    qualification: Qualification;
+    taxonomies: Taxonomy[];
+
+    //Location Panel
+    facilities: Facility[];
+}
+
+class Taxonomy {
+    taxonomyType: string;
+    taxonomyCode: string;
+    status: string;
+}
+
+class Qualification {
+    professionalDegreeCode: string;
+    professionalDegreeSchool?: string;
+    certificateNumber?: string;
+    graduationYear?: number;
+}
+
+class ContractedPartner {
+    partnerName: string;
+}
+
+class Facility {
+    facilityId?: string;
+    facilityName?: string;
+    facilityType: string;
+    practiceType: string;
+    dhcsSiteId?: string;
+    lastFsrDate?: string;
+    email: string;
+    phones: Phone[];
+    hoursOfOperation: HoursOfOperation[];
+    age: Age;
+    address: Address;
+}
+
+class Phone {
+    type: string;
+    number: string;
+}
+
+class HoursOfOperation {
+    day: string;
+    open: string;
+    close: string;
+}
+
+class Age {
+    min: number;
+    max: number;
+}
+
+class Address {
+    name: string;
+    addressLine1: string;
+    city: string;
+    country: string;
+    state: string;
+    zipCode: string;
+}
+
+
+
+
+
+
---
+++ src/app/shared/intake-interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
+import 'rxjs/add/observable/throw'
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class IntakeInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        
+        let headers = new HttpHeaders();
+        headers = headers.append("Content-type", "application/json");
+        headers = headers.append("Authorization", "Basic " + btoa("966b9d649e724d3887c6db7c1468af53:489402D0779543F8aDaFd10c6d4A03D4"));
+
+        const authReq = req.clone({ headers: headers });
+
+        return next.handle(authReq)
+            .catch((error, caught) => {
+                console.log(error);
+                return Observable.throw(error);
+            }) as any;
+    }
+}
