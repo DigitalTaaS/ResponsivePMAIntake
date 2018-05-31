cc93360bf75054463f4064989fb76e1ce593baa7
Anand Selvaraj <sanand.net@gmail.com>
2018-05-30 19:04:12 -0500

    Rxjs import fix

--- src/app/shared/intake-interceptor.ts
+++ src/app/shared/intake-interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs';
 import 'rxjs/add/observable/throw'
 import 'rxjs/add/operator/catch';
 
