9359dab3110350eb28949ee07a21a15448eeb97a
Thad Blankenship <>
2018-06-10 17:41:54 -0500

    Updateated failure popup

--- src/app/contractphysician/confirmation.modal.ts
+++ src/app/contractphysician/confirmation.modal.ts
@@ -6,36 +6,7 @@ import { Router } from '@angular/router';
     selector: 'confirmation-modal',
     templateUrl: './confirmation.modal.html',      
     encapsulation: ViewEncapsulation.None,
-    styles: [`
-    .modal-content {
-        background-color: #B1E6E9;
-        color: white;
-        
-    }
-
-    .error-modal .modal-content {
-        background-color: #B09EC1;
-        color: white;
-        
-    }
-
-    .success-modal{
-        height:300px;
-        padding-top:75px;
-    }
-
-    .modal-backdrop.fade {
-        opacity: 0.5;
-    }
-
-    .modal-open .modal {
-        opacity: 1;
-    }
-    .p-text{
-        font-size:35px;
-        font-family: ArialMT, Regular;
-    }
-    `]
+    styleUrls: ['confirmation.modal.css']
 })
 export class ConfirmationModal {
 
